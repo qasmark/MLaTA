9ca5b31d17a0784f43af065f08e53e947a97258e
qasmark <99826972+qasmark@users.noreply.github.com>
2023-05-28 22:36:23 +0300

    Inversion: added python code for tests,
    added tests

--- labs/Inversion/Inversion.cpp
+++ labs/Inversion/Inversion.cpp
@@ -16,71 +16,106 @@
 0 0 1 3 2
 	Игнатьев Влад, ПС-22
 	Visual Studio Community 2022
+
+Суть решения:
+    Используется класс дерева Фенвика, чтобы ффективно обновлять значения и выполнять запросы 
+    суммы на префиксном префиксном отрезке массива. До этого я пытался сделать другими методами (MergeSort, RadixSort),
+    Однако это не дало такой эффективности, как использование дерева Фенивка в этой задачи. Кстати, нашел в интернете, 
+    что дерево Фенвика по-другому называется бинарное индексированное дерево :)
+    В классе используется методы Update и Query, затем идет подсчет пар через std::vector<std::pair<int, int>> elements(N)
+    Самое трудное было разобраться с перебором, а так все работает.
+    Также прилагаю несколько своих тестов и генератор тестов. Можете посмотреть его
+
+    На тесте cin5.txt время выполнения программы 2.944 секунды в Debug, а в сборке Release 0.33, доказательства будут в word
 */
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
-#include <fstream>
+#include <ctime>
 
-using namespace std;
-
-void update(vector<int>& bit, int i, int val)
+class FenwickTree
 {
-    while (i < bit.size())
-    {
-        bit[i] += val;
-        i += i & -i;
-    }
-}
+public:
+    FenwickTree(int size) : tree(size + 1, 0) {}
 
-int sum(vector<int>& bit, int i)
-{
-    int sm = 0;
-    while (i > 0)
+    void Update(int index, int value)
     {
-        sm += bit[i];
-        i -= i & -i;
+        while (index < tree.size())
+        {
+            tree[index] += value;
+            index += index & -index;
+        }
     }
-    return sm;
+
+    int Query(int index)
+    {
+        int sum = 0;
+        while (index > 0)
+        {
+            sum += tree[index];
+            index -= index & -index;
+        }
+        return sum;
+    }
+
+private:
+    std::vector<int> tree;
+};
+
+std::vector<int> CountInversions(const std::vector<int>& permutation, long long& inversionCount)
+{
+    int N = permutation.size();
+    std::vector<int> inversions(N);
+    FenwickTree tree(N);
+
+    std::vector<std::pair<int, int>> elements(N);
+    for (int i = 0; i < N; ++i)
+        elements[i] = std::make_pair(permutation[i], i);
+
+    std::sort(elements.begin(), elements.end());
+
+    for (int i = N - 1; i >= 0; --i)
+    {
+        inversions[elements[i].second] = tree.Query(elements[i].second);
+        tree.Update(elements[i].second + 1, 1);
+    }
+
+    inversionCount = 0;
+    for (int i = 0; i < inversions.size(); ++i)
+        inversionCount += inversions[i];
+
+    return inversions;
 }
 
 int main()
 {
+    time_t start = clock();
+    std::ifstream inputFile("cin.txt");
+    std::ofstream outputFile("cout.txt");
 
-    int n;
-    cin >> n;
+    int N;
+    inputFile >> N;
 
-    vector<int> nums(n);
-    vector<int> sortedNums(n);
+    std::vector<int> permutation(N);
+    for (int i = 0; i < N; ++i)
+        inputFile >> permutation[i];
 
-    for (int i = 0; i < n; i++)
+    long long inversionCount;
+    std::vector<int> inversions = CountInversions(permutation, inversionCount);
+
+    outputFile << inversionCount << std::endl;
+    for (int i = 0; i < inversions.size(); ++i)
     {
-        cin >> nums[i];
-        sortedNums[i] = nums[i];
+        outputFile << inversions[i];
+        if (i != inversions.size() - 1)
+            outputFile << " ";
     }
+    outputFile << std::endl;
 
-    sort(sortedNums.begin(), sortedNums.end());
-    vector<int> index(n);
-    for (int i = 0; i < n; i++)
-    {
-        index[i] = lower_bound(sortedNums.begin(), sortedNums.end(), nums[i]) - sortedNums.begin();
-    }
+    inputFile.close();
+    outputFile.close();
 
-    vector<int> bit(n + 1, 0);
-    long long inv_count = 0;
-    vector<int> inversions(n, 0);
-    for (int i = n - 1; i >= 0; i--)
-    {
-        inv_count += sum(bit, index[i] - 1);
-        inversions[i] = inv_count;
-        update(bit, index[i], 1);
-    }
-
-    cout << inv_count << endl;
-    for (int k = 0; k < inversions.size(); k++)
-    {
-        cout << inversions[k] << " ";
-    }
-
+    std::cout << "The time execution is " << (double)(clock() - start) / CLOCKS_PER_SEC << " s." << std::endl;
     return 0;
 }
--- labs/Inversion/task.py
+++ labs/Inversion/task.py
@@ -3,6 +3,6 @@ size = randint(5, 500000)
 lst = [i for i in range(1, size)]
 shuffle(lst)
 
-with open('input.txt', 'w') as f:
+with open('cin5.txt', 'w') as f:
     f.write(str(size) + '\n')
     f.write(' '.join(map(str, lst)))
