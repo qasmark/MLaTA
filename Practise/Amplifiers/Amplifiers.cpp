f78ede8af1ee7f2d3e5469421471a2086e799c39
qasmark <99826972+qasmark@users.noreply.github.com>
2023-06-07 23:43:30 +0300

    Practise: added last work with tests

---
+++ Practise/Amplifiers/Amplifiers.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	unsigned long long N;
+	std::cin >> N;
+
+	if (N % 2 == 0)
+	{
+		std::cout << "No\n";
+		return 0;
+	}	
+	N >>= 1;
+
+	std::vector<unsigned long long> result;
+	while (N != 0)
+	{
+		result.push_back((N & 1) + 1);
+		N >>= 1;
+	}
+
+	std::cout << result.size() << '\n';
+	std::reverse(result.begin(), result.end());
+
+	for (auto i : result)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << '\n';
+
+
+	return 0;
+}
